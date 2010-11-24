f649705af307f2df2294747117ae510ca39ebc05
strikegamer <strikegamer@gmail.com>
2010-11-24 14:19:12 -0200

    Added achievements page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :links
 
   map.resources :players
+  
+  map.resources :achievements
 
   # The priority is based upon order of creation: first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101122012613) do
+ActiveRecord::Schema.define(:version => 20101124113129) do
+
+  create_table "achievements", :force => true do |t|
+    t.integer  "player_id"
+    t.string   "title"
+    t.string   "detail"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "links", :force => true do |t|
     t.integer  "user_id"
