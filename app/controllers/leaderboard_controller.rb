3c8e2db5ec316ae8fe89c5c8805a00b12aec5200
gabi <gabigomide@gmail.com>
2010-11-25 15:27:47 -0300

    Include the login require on leaderboard_controller; exclude the method on leaderboard_helper and put on player_model; fixed the scenario for to pass with the shares column on the table (leaderboard page).

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,4 +1,6 @@
 class LeaderboardController < ApplicationController  
+  before_filter :login_required
+  
   def index
     @players = Player.find(:all, :order => "points DESC")
   end
--- app/helpers/leaderboard_helper.rb
+++ app/helpers/leaderboard_helper.rb
@@ -1,6 +1,4 @@
 module LeaderboardHelper
-	def count_shares (player)
-		player.tasks.count + player.links.count + player.xps.count
-	end
+	
 end
 
