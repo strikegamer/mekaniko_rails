f1e667e6b564d78b0ca8b6e67a3f7913ab06667b
bmsatierf <bmsatierf@gmail.com>
2010-11-26 08:13:01 -0200

    Minor changes on Comments creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,14 @@ class CommentsController < ApplicationController
    before_filter :login_required
    
    def create
-      @action = case params[:action_type]
-         when "link" then Link.find(params[:link_id])
-         when "task" then Task.find(params[:task_id])
+      @share = case params[:type]
+         when "Link" then Link.find(params[:link_id])
+         when "Task" then Task.find(params[:task_id])
          else Xp.find(params[:xp_id])
       end
 
-      @comment = @action.comments.create!(params[:comment].merge(:player_id => current_user.player.id))
+      @comment = @share.comments.create!(params[:comment].merge(:player_id => current_user.player.id))
 
-      redirect_to @action
+      redirect_to @share
    end
 end
